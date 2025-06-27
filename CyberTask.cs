c8d45ffa99213d40eb8c759b0aae0c8a006bcfee
ST10462427 <166802244+ST10462427@users.noreply.github.com>
2025-06-27 16:14:50 +0000

    CyberTask.cs
    file upload

---
+++ CyberTask.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TaskManagementApp
+{
+    public class CyberTask
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime? ReminderDate { get; set; }
+        public bool IsCompleted { get; set; }
+
+        public string Display => $"{Title} {(IsCompleted ? "(Completed)" : "")}";
+    }
+}
+
